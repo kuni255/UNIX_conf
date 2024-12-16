a049433727cc76924b799ba7df4e1d4b4d962e68
Hayato Kuniie <hayato@arcopizz.net>
2024-12-16 22:51:19 +0900

    Fish configuration

--- config.fish
+++ config.fish
@@ -1,2 +1,6 @@
 set -gx EDITOR vi
 starship init fish | source
+
+source /opt/homebrew/opt/modules/init/fish
+source /opt/homebrew/opt/modules/init/fish_completion
+set -gx MODULEPATH $MODULEPATH:$HOME/.module
